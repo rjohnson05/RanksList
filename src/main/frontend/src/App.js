f4cb0cbfbd9077cb69006dd4ce9d7ac246252b89
Hank Rugg <hrugg@carroll.edu>
2023-11-27 11:27:35 -0700

    updated README and deleted goals page

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Home from './MainPage/Home';
 import RegistrationForm from './LoginPage/RegistrationForm'
 import LoginForm from './LoginPage/LoginForm';
-import Goals from './MainPage/Goals'
 import {HashRouter, Navigate, Route, Routes} from "react-router-dom";
 import CreateAdForm from "./CreateAdPage/CreateAdForm";
 import IndividualGoals from "./IndividualGoalsPage/IndividualGoals";
@@ -23,7 +22,6 @@ export default function App() {
                     <Route path='login' element={<LoginForm />} />
                     <Route path='register' element={<RegistrationForm />} />
                     <Route path='create_ad' element={<CreateAdForm />} />
-                    <Route path='goals' element={<Goals />} />
                     <Route path='starred_ads' element={<StarredAds />} />
                     <Route path='edit_ad/*' element={<EditAd />} />
                     <Route path={'my_ads'} element={<MyCreatedAds />} />
