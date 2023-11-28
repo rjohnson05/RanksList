7ec7ea905e3b333222bb7e7d6fed67e0ff49ad39
Ryan Johnson <rjohnson@carroll.edu>
2023-11-28 00:07:45 -0700

    Minor cosmetic edits

--- src/main/frontend/src/LoginPage/EditPasswordForm.js
+++ src/main/frontend/src/LoginPage/EditPasswordForm.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import {useLocation, useNavigate} from "react-router-dom"
+import React, {useState} from 'react';
+import {useNavigate} from "react-router-dom"
 import { useForm } from "react-hook-form"
 import axios from "axios";
 import NavBar from "../MainPage/NavBar";
